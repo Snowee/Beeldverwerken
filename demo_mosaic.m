8c06bdd58e1ead68f5a041cd7af4f9a884a0a506
Steve Nowee <steve_nowee@hotmail.com>
2012-12-02 16:41:36 +0100

    demo_mosaic comments toegevoegd

--- demo_mosaic.m
+++ demo_mosaic.m
@@ -12,8 +12,10 @@ T = RANSAC(f1,f2);
 % T = maketform('projective',xy', xaya')
 % T = maketform('projective',createProjectionMatrix(xy', xaya'))
 
+% Transform image f1 with the transformation matrix
 [x y] = tformfwd(T,[1 size(f1,2)], [1 size(f1,1)]);
 
+% Print the images
 xdata = [min(1,x(1)) max(size(f2,2),x(2))];
 ydata = [min(1,y(1)) max(size(f2,1),y(2))];
 f12 = imtransform(f1,T,'Xdata',xdata,'YData',ydata);
