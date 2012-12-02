693e94e857eb212a305c0e19505722af4ff0e675
Steve Nowee <steve_nowee@hotmail.com>
2012-12-02 16:30:28 +0100

    Ransac Compleet comments
    + plaatjes voor verslag

--- RANSAC.m
+++ RANSAC.m
@@ -30,48 +30,65 @@ F2Coordinates = [X2F;Y2F;homogeneousAppend]';
 % Start the best number of matches at 0
 BestMatches = 0;
 for j = 1:100
+	% Matrix with two random integer values between 1 and number of matchpoints
     randSel = randsample(size(DMatch,2),2)
+	% Matrix putting 1 to the number of matchpoints in random order
     perm = randperm(size(DMatch,2));
+	% Pick two descriptor indices with random values out of a random matrix
     selection = [perm(randSel(1,1)),perm(randSel(2,1))];
-
+	% Take the corresponding coordinates to the descriptors
     XF1 = F1Coordinates(selection,1);
     YF1 = F1Coordinates(selection,2);
+	% If and while equal coordinate points are found keep taking 
+	% other random descriptors and their coordinates
     while XF1(1,1) == XF1(2,1) & YF1(1,1) == YF1(2,1)
-        randSel = randsample(size(DMatch,2),2)
+        randSel = randsample(size(DMatch,2),2);
         selection = [perm(randSel(1,1)),perm(randSel(2,1))];
         XF1 = F1Coordinates(selection,1);
         YF1 = F1Coordinates(selection,2);
     end
+	% Combine x and y coordinates
     CoordinatesF1 = [XF1,YF1];
     
+	% Take the corresponding coordinates to the descriptors
     XF2 = F2Coordinates(selection,1);
     YF2 = F2Coordinates(selection,2);
-    if XF2(1,1) == XF2(2,1) & YF2(1,1) == YF2(2,1)
-        selection = perm(3:4);
+	% If and while equal coordinate points are found keep taking 
+	% other random descriptors and their coordinates
+    while XF2(1,1) == XF2(2,1) & YF2(1,1) == YF2(2,1)
+		randSel = randsample(size(DMatch,2),2);
+        selection = [perm(randSel(1,1)),perm(randSel(2,1))];
         XF2 = F2Coordinates(selection,1);
         YF2 = F2Coordinates(selection,2);
     end
+	% Combine x and y coordinates
     CoordinatesF2 = [XF2,YF2];
 
+	% Create a tform with the right transformation matrix
+	% that transforms F1 coords to F2 coords
     form = cp2tform(CoordinatesF1,CoordinatesF2,'nonreflective similarity')
+	% Calculate the F2 coordinates with the transformation matrix
+	% and the F1 coordinates
     [calcF2X, calcF2Y] = tformfwd(form, X1F, Y1F);
-    calcF2Coordinates = [calcF2X;calcF2Y;homogeneousAppend];
-
-    calcF2Coordinates(:,1) = calcF2Coordinates(:,1) ./ calcF2Coordinates(:,3);
-    calcF2Coordinates(:,2) = calcF2Coordinates(:,2) ./ calcF2Coordinates(:,3);
-    calcF2Coordinates(:,3) = calcF2Coordinates(:,3) ./ calcF2Coordinates(:,3);
-    calcF2Coordinates = calcF2Coordinates';
+	
+    calcF2Coordinates = [calcF2X;calcF2Y;homogeneousAppend]';
+    
+    % Calculate the elementwise difference between the
+    % calculated F2 coordinates and the real F2 coordinates
     Diff = abs(F2Coordinates - calcF2Coordinates);
-
+    % Set the max difference and initialize the number of matches at zero
     Error = 2;
     Matches = 0;
-
+    % Loop through the difference matrix and if both the x and y
+    % coordinates are smaller than the max error add 1 to the number of
+    % matches
     for i = 1:size(Diff,1)
         if Diff(i,1) < Error & Diff(i,2) < Error
             Matches = Matches + 1;
         end
     end
-    
+    % Save the best number of matches and with it the tform and
+    % tranformation matrix that made the best number of matches
     if Matches > BestMatches
         BestMatches = Matches;
         Bestform = form;
