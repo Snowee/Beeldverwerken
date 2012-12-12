b62d78b28ea84515cfe6218913648a4e3df88e2f
Steve Nowee <steve_nowee@hotmail.com>
2012-12-12 19:37:15 +0100

    uitbreiding coins

--- coins.m
+++ coins.m
@@ -1,10 +1,32 @@
-image = im2double(imread('eight.tif'))
+image = im2double(imread('eight.tif'));
 
-binary = image< isodata('eight.tif')
+binary = image< isodata('eight.tif');
 
 se = zeros(7,7);
 se (1 ,1)=1;
 
 C = imdilate(binary,se);
 
-oimshow(image,binary,C)
+figure
+oimshow(image,binary,C);
+
+
+figure
+subplot(2,3,1)
+imshow(binary)
+N = [0,3,15,30,60];
+for i = 2:5
+    subplot(2,3,i);
+    C=imdilate(binary,strel('square',N(i)));
+    imshow(C)
+end
+figure
+subplot(2,3,1)
+imshow(binary)
+for i = 2:5
+    subplot(2,3,i);
+    C=imdilate(binary,strel('square',N(i)));
+    D=imerode(C,strel('square',N(i)));
+    imshow(D)
+    oimshow(zeros(size(binary),binary,D))
+end
