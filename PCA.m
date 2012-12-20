f5a6bb0885009cc224f712727d28a2744688332d
Steve Nowee <steve_nowee@hotmail.com>
2012-12-20 19:27:14 +0100

    blabla

--- PCA.m
+++ PCA.m
@@ -1,5 +1,9 @@
-function [  E, gTest, g, gComp, bestMatch, accuracy ] = PCA( train, test, X, XTest, MeanIm, d, testim )
-load omni.mat;
+% Function for applying PCA to a robot positioning task
+% Takes a trainingset, testset, matrix of all train images minus the mean
+% image, matrix of all test images minus the mean image, the mean image and
+% the number of dimensions used for PCA.
+% Returns an eigenvector matrix E, The gTest
+function [  E, gTest, g, gComp, bestMatch, accuracy ] = PCA( train, test, X, XTest, MeanIm, d )
 [V,D] = eigs( X'*X, d );
 
 E = X*V*inv(D);
@@ -7,10 +11,10 @@ for i = 1:size(E,2)
     E(:,i) = E(:,i)./norm( E(:,i) );
 end
 
-g = E'*X;
 
-EG = E*g;
-% Plot eigenvalues
+%--------------------------------------------------
+%- Code to plot d eigenvalues:
+%--------------------------------------------------
 % figure
 % for i = 1:size(train,2)/6
 %     eigenvalues(i) = D(i,i)
@@ -22,43 +26,56 @@ EG = E*g;
 %     subplot( 2, 5, i )
 %     imshow( reshape( E(:,i), 112, [] ), [] )
 % end
+% -------------------------------------------------
  
-% Print original image and estimate by eigenvectors
+%-----------------------------------------------------------------
+%- Code to print an original image and estimate by d eigenvectors
+%-----------------------------------------------------------------
 % figure
 % subplot(2,1,1);
 % imshow( reshape( X(:,1) + MeanIm, 112, 150 ) )
 % subplot( 2, 1, 2 );
 % imshow( reshape( EG(:,1) + MeanIm, 112, 150 ) )
+%-----------------------------------------------------------------
 
+% Compute the vectors of the components of X and XTest in eigenspace g and
+% gTest consist of 300 and 250 vectors with each d elements
+g = E'*X;
 gTest = E'*XTest; 
-% gTestIm = gTest(:,testim);
 
+% Make the correlation between all g vectors and all gTest vectors
 gComp = zeros(size(train,2),size(test,2));
 for j = 1:size( gTest, 2 )
     for i = 1:size( g, 2 )
-    % gDiff(:,i) = g(:,i)-gTestIm;
-    % gSum(1,i) = sum(gDiff(:,i));
-    gComp(i,j) = dot( g(:,i), gTest(:,j) );
+        gComp(i,j) = dot( g(:,i), gTest(:,j) );
     end
 end
+
+% Find all the rows that are a best match to a testimage.
 bestMatch = zeros(1,size(XTest,2));
 for i = 1:size(gComp,2)
     [row, column] = find(gComp == max(gComp(:,i)));
     bestMatch(1,i) = row;
 end
 
+% Get all positions of the best match images
 matchedPositions = zeros(size(bestMatch,2),2);
 for i = 1:size(bestMatch,2)
     matchedPositions(i,:) = train{bestMatch(1,i)}.position;    
 end
 
+% Get all positions of the test images
 testPositions = zeros(size(test,2),2);
 for i = 1:size(test,2)
     testPositions(i,:) = test{i}.position;
 end
 
+% Initialize a counter for the accuracy
 accuracyCounter = 0;
 
+% Calculate the Euclidean distance between each testposition and
+% matchedposition. If the Euclidean distance is less than 150 add 1
+% to the accuracy counter
 for i = 1:size(testPositions,1)
    xDifference = (matchedPositions(i,1)-testPositions(i,1))^2;
    yDifference = (matchedPositions(i,2)-testPositions(i,2))^2;
@@ -68,24 +85,22 @@ for i = 1:size(testPositions,1)
        accuracyCounter = accuracyCounter + 1;
    end
 end
-
+% Calculate the accuracy of the robot positioning task with d dimensions
 accuracy = accuracyCounter/size(test,2);
 
+%-----------------------------------------------------------------
+%- Code for plotting all testpositions and all matched positions
+%-----------------------------------------------------------------
 % Xtest = testPositions(:,1);
 % Ytest = testPositions(:,2);
 % 
-% size(MatchedPositions,1)
-% Xtrain = MatchedPositions(:,1);
-% Ytrain = MatchedPositions(:,2);
+% size(matchedPositions,1)
+% Xtrain = matchedPositions(:,1);
+% Ytrain = matchedPositions(:,2);
 % train = plot(Xtrain,Ytrain,'o');
 % hold on
 % test = plot(Xtest,Ytest,'o');
 % set(test,'Color', 'red');
 % hold off
-
-
-% gComp
-% end
-% [ row, column ] = find( gComp == max( gComp ) );
-% match = column;
-% end
+%-----------------------------------------------------------------
+end
--- dataSplit.m
+++ dataSplit.m
@@ -1,26 +1,30 @@
+% Function for preprocessing the dataset with robot positioning images
+% Takes a dataset
+% Returns a trainingset, testset, matrix of train images minus mean image,
+% matrix of test images minus mean image an the mean image
 function [Train, Test, X, XTest, meanIm] = dataSplit(data)
+% Load the dataset, creating struct: images
 load(data);
+
+% Reshaping all images in the dataset to vectors
 for i = 1:size(images,2)
    sizes = size(images{i}.img,2)*size(images{i}.img,1);
    images{i}.img = reshape(images{i}.img, sizes,1);
 end
 
+% Divide the dataset into training and testset
 Train = images(1:300);
 Test = images(301:550);
 
+% Calculate the mean image meanIm of the trainingset
 TrainMean = zeros(16800,1);
 for i = 1:300
     TrainMean = TrainMean + Train{1,i}.img;
 end
 meanIm = TrainMean ./ 300;
 
-
-% allTrain =[];
-% x = zeros(16800,300);
-% for i = 1:size(train,2)
-%     allTrain = [allTrain,train{1,i}.img];
-% end
-% meanIm = mean(Train.img,2)
+% Create the matrices with the training images minus the mean image and the
+% test images minus the mean image
 X = zeros(16800,300);
 XTest = zeros(16800,250);
 for i = 1:size(Train,2)
